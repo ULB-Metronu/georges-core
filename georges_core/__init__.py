8826b2fbbd9dd61213e974828b5c2791f4562669
Cédric Hernalsteens <cedric.hernalsteens@ulb.be>
2019-09-12 11:54:32 +0200

    Refactoring the visualization module to move into georges_core
    Signed-off-by: Cédric Hernalsteens <cedric.hernalsteens@ulb.be>

--- georges_core/__init__.py
+++ georges_core/__init__.py
@@ -4,3 +4,4 @@ from .units import ureg, Q_
 from .kinematics import Kinematics, ZgoubiKinematicsException
 from .frame import Frame, FrameException
 from .patchable import Patchable
+from .vis import Artist, GnuplotArtist, PlotlyArtist
---
+++ georges_core/vis/__init__.py
@@ -0,0 +1,3 @@
+from .artist import Artist
+from .gnuplot import GnuplotArtist
+from .plotly import PlotlyArtist
