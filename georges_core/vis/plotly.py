5fbcf3075f42bd6073b0c3145a062269887dab65
Cédric Hernalsteens <cedric.hernalsteens@ulb.be>
2019-11-12 15:03:13 +0100

    Add features to the plotly artist class.
    Signed-off-by: Cédric Hernalsteens <cedric.hernalsteens@ulb.be>

--- georges_core/vis/plotly.py
+++ georges_core/vis/plotly.py
@@ -2,10 +2,13 @@
 TODO
 """
 from __future__ import annotations
-from typing import Optional, Mapping
+from typing import TYPE_CHECKING, Optional, Mapping
+import numpy as _np
 import plotly.offline
 import plotly.graph_objs as go
 from .artist import Artist as _Artist
+if TYPE_CHECKING:
+    import pandas as _pd
 
 
 class PlotlyArtist(_Artist):
@@ -143,9 +146,16 @@ class PlotlyArtist(_Artist):
         """A proxy for plotly.graph_objs.Histogram"""
         self._data.append(go.Histogram(*args, **kwargs))
 
+    def uproot_histogram(self, histogram, **kwargs):
+        _ = histogram.numpy()
+        self.bar(x=_[1], y=_[0], error_y={'array': _np.sqrt(histogram.variances)}, **kwargs)
+
     def histogram2d(self, *args, **kwargs):
         """A proxy for plotly.graph_objs.Histogram2d"""
         self._data.append(go.Histogram2d(*args, **kwargs))
+
+    def bar(self, *args, **kwargs):
+        self._data.append(go.Bar(*args, **kwargs))
 
     def scatter(self, *args, **kwargs):
         """A proxy for plotly.graph_objs.Scatter ."""
@@ -154,3 +164,126 @@ class PlotlyArtist(_Artist):
     def scatter3d(self, *args, **kwargs):
         """A proxy for plotly.graph_objs.Scatter3d ."""
         self._data.append(go.Scatter3d(*args, **kwargs))
+
+    def plot_cartouche(self,
+                       beamline_survey: _pd.DataFrame,
+                       vertical_position: float = 1.2,
+                       unsplit_bends: bool = True,
+                       skip_elements: Optional[list] = None,
+                       ):
+        """
+
+        Args:
+            beamline_survey:
+            vertical_position:
+            unsplit_bends:
+            skip_elements:
+
+        Returns:
+
+        """
+        skip_elements = skip_elements or []
+
+        def do_sbend(at_entry: float, at_exit: float, polarity: float):
+            length = at_exit - at_entry
+            if polarity >= 0.0:
+                path = f"M{at_entry},{vertical_position + 0.1} " \
+                       f"H{at_exit} " \
+                       f"L{at_exit - 0.15 * length},{vertical_position - 0.1} " \
+                       f"H{at_exit - 0.85 * length} " \
+                       f"Z"
+            else:
+                path = f"M{at_entry + 0.15 * length},{vertical_position + 0.1} " \
+                       f"H{at_exit - 0.15 * length} " \
+                       f"L{at_exit},{vertical_position - 0.1} " \
+                       f"H{at_entry} " \
+                       f"Z"
+            self.shapes.append(
+                {
+                    'type': 'path',
+                    'xref': 'x',
+                    'yref': 'paper',
+                    'path': path,
+                    'line': {
+                        'width': 0,
+                    },
+                    'fillcolor': '#0000FF',
+                },
+            )
+
+        colors = {
+            'ELEMENT': '#AAAAAA',
+            'RCOL': '#11EE11',
+            'ECOL': '#1111EE'
+        }
+        self.shapes.append(
+            {
+                'type': 'line',
+                'xref': 'paper',
+                'yref': 'paper',
+                'x0': 0,
+                'y0': vertical_position,
+                'x1': 1,
+                'y1': vertical_position,
+                'line': {
+                    'color': 'rgb(150, 150, 150)',
+                    'width': 2,
+                },
+            },
+        )
+        accumulate = False
+        accumulator = {}
+        for i, e in beamline_survey.iterrows():
+            if e['TYPE'].upper() not in ('QUADRUPOLE', 'SBEND', 'ELEMENT', 'RCOL', 'ECOL'):
+                continue
+            if i in skip_elements:
+                continue
+            if unsplit_bends and accumulate and (e['TYPE'].upper() not in ('SBEND',) or i != accumulator['name']):
+                accumulate = False
+                do_sbend(accumulator['at_entry'], accumulator['at_exit'], accumulator['polarity'])
+                accumulator = {}
+            if e['TYPE'].upper() in ('ELEMENT', 'RCOL', 'ECOL'):
+                self.shapes.append(
+                    {
+                        'type': 'rect',
+                        'xref': 'x',
+                        'yref': 'paper',
+                        'x0': e['AT_ENTRY'],
+                        'y0': vertical_position + 0.1,
+                        'x1': e['AT_EXIT'],
+                        'y1': vertical_position - 0.1,
+                        'line': {
+                            'width': 0,
+                        },
+                        'fillcolor': colors[e['TYPE'].upper()],
+                    },
+                )
+            if e['TYPE'].upper() == 'QUADRUPOLE':
+                self.shapes.append(
+                    {
+                        'type': 'rect',
+                        'xref': 'x',
+                        'yref': 'paper',
+                        'x0': e['AT_ENTRY'],
+                        'y0': vertical_position if e['K1'] > 0 else vertical_position - 0.1,
+                        'x1': e['AT_EXIT'],
+                        'y1': vertical_position + 0.1 if e['K1'] > 0 else vertical_position,
+                        'line': {
+                            'width': 0,
+                        },
+                        'fillcolor': '#FF0000',
+                    },
+                )
+            if e['TYPE'].upper() == 'SBEND':
+                if unsplit_bends:
+                    if accumulate is False:
+                        accumulate = True
+                        accumulator['name'] = i
+                        accumulator['polarity'] = e['B']
+                        accumulator['at_entry'] = e['AT_ENTRY']
+                        continue
+                    if accumulate is True:
+                        accumulator['at_exit'] = e['AT_EXIT']
+                        continue
+                else:
+                    do_sbend(e['AT_ENTRY'], e['AT_EXIT'])
