fd44173120465a110260b4070a6d9b33fd97e478
Cédric Hernalsteens <cedric.hernalsteens@ulb.be>
2020-03-22 13:03:42 +0100

    Add BDSIM output in georges-core (moved from georges).
    Signed-off-by: Cédric Hernalsteens <cedric.hernalsteens@ulb.be>

--- georges_core/codes_io/__init__.py
+++ georges_core/codes_io/__init__.py
@@ -1,2 +1,3 @@
 from .madx import load_madx_twiss_table, load_madx_twiss_headers, get_twiss_values
 from .transport import load_transport_input_file, transport_element_factory
+from .bdsim import Output, BDSimOutput, ReBDSimOutput, ReBDSimOpticsOutput
