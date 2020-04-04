4f3c977294a4701dd8106f95e940389db94af974
Cédric Hernalsteens <cedric.hernalsteens@ulb.be>
2020-04-04 13:03:27 +0200

    WIP - Refactor the way the samplers are processed

--- georges_core/codes_io/bdsim.py
+++ georges_core/codes_io/bdsim.py
@@ -208,7 +208,7 @@ class Output(metaclass=OutputType):
                 setattr(instance, f"toggle_{k}", toggle(k).__get__(instance))
             return instance
 
-        def __init__(self, parent: Output.Tree):
+        def __init__(self, parent: Output.Tree, branch_name: Optional[str] = None):
             """
             A representation of a ROOT Branch.
 
@@ -216,9 +216,7 @@ class Output(metaclass=OutputType):
                 parent: the `Tree` to which the branch is attached
             """
             self._parent: Output.Tree = parent
-            b = self.BRANCH_NAME \
-                if self.BRANCH_NAME is not None \
-                else self.__class__.__name__
+            b = branch_name or self.BRANCH_NAME or self.__class__.__name__
             if len(b) > 0:
                 self._branch = parent[b]
             else:
@@ -457,53 +455,6 @@ class BDSimOutput(Output):
                 'eventGeneratorParticles': [True, None],
             }
 
-        @staticmethod
-        def load_bdsim_kinematics(filename: str, path: str = '.') -> _Kinematics:
-            """
-
-            Args:
-                filename:
-                path:
-
-            Returns:
-
-            """
-            _: _pd.DataFrame = georges.bdsim.BDSimOutput(os.path.join(path, filename)).beam.df
-            particle_name = _["particleName"].values[0].capitalize()
-            p = getattr(_particles, particle_name if particle_name != 'Default' else 'Proton')
-            return _Kinematics(_["E0"].values[0] * _ureg.GeV, kinetic=False, particle=p)
-
-        @staticmethod
-        def load_bdsim_beam_distribution(filename: str, path: str = '.') -> _pd.DataFrame:
-            """
-
-            Args:
-                filename:
-                path:
-                with_units:
-
-            Returns:
-
-            """
-            beam_distribution: _pd.DataFrame = georges.bdsim.BDSimOutput(os.path.join(path, filename)).event.primary.df
-
-            for c in beam_distribution.columns:
-                try:
-                    beam_distribution[c] = beam_distribution[c].apply(float)
-                except ValueError:
-                    pass
-
-            beam_distribution['kineticEnergy'] = beam_distribution['kineticEnergy'].apply(lambda e: e * _ureg.GeV)
-            beam_distribution['p'] = beam_distribution['kineticEnergy'].apply(
-                lambda e: _Kinematics(e, kinetic=True).to_momentum()
-            )
-            beam_distribution.rename(columns={
-                "xp": "px",
-                "yp": "py"
-            }, inplace=True)
-            beam_distribution.columns = map(str.upper, beam_distribution.columns)
-            return beam_distribution
-
     class Geant4Data(Output.Tree):
         # https://github.com/scikit-hep/uproot/issues/468
         ...
@@ -590,57 +541,6 @@ class BDSimOutput(Output):
 
             return self._df
 
-        @staticmethod
-        def load_bdsim_model(filename: str, path: str = '.', with_units: bool = True) -> _pd.DataFrame:
-            """
-
-            Args:
-                filename:
-                path:
-                with_units:
-
-            Returns:
-
-            """
-            _BDSIM_TO_MAD_CONVENTION: Mapping[str, str] = {
-                'Rcol': 'RectangularCollimator',
-                'Ecol': 'EllipticalCollimator',
-            }
-
-            _: _pd.DataFrame = georges.bdsim.BDSimOutput(os.path.join(path, filename)).model.df
-
-            for c in _.columns:
-                try:
-                    _[c] = _[c].apply(float)
-                except ValueError:
-                    pass
-
-            _['CLASS'] = _['TYPE'].apply(str.capitalize)
-            _['CLASS'] = _['CLASS'].apply(lambda e: _BDSIM_TO_MAD_CONVENTION.get(e, e))
-
-            ## TMP
-            for i, line in _.iterrows():
-                if line["CLASS"] == "RectangularCollimator" or line["CLASS"] == "Dump":
-                    _.at[i, "APERTYPE"] = "rectangular"
-                if line["CLASS"] == "EllipticalCollimator":
-                    _.at[i, "APERTYPE"] = "elliptical"
-
-            if with_units:
-                _['L'] = _['L'].apply(lambda e: e * _ureg.m)
-                _['APERTURE1'] = _['APERTURE1'].apply(lambda e: e * _ureg.m)
-                _['APERTURE2'] = _['APERTURE2'].apply(lambda e: e * _ureg.m)
-                _['APERTURE'] = _[['APERTURE1', 'APERTURE2']].apply(list, axis=1)
-                _['K1'] = _['K1'].apply(lambda e: e * _ureg.m ** -2)
-                _['K1S'] = _['K1S'].apply(lambda e: e * _ureg.m ** -2)
-                _['K2'] = _['K2'].apply(lambda e: e * _ureg.m ** -3)
-                _['E1'] = _['E1'].apply(lambda e: e * _ureg.radian)
-                _['E2'] = _['E2'].apply(lambda e: e * _ureg.radian)
-                _['HGAP'] = _['HGAP'].apply(lambda e: e * _ureg.meter)
-                _['TILT'] = _['TILT'].apply(lambda e: e * _ureg.radian)
-                _['B'] = _['B'].apply(lambda e: e * _ureg.T)
-
-            return _
-
     class Run(Output.Tree):
         def __getattr__(self, item):
             if item in (
@@ -681,8 +581,8 @@ class BDSimOutput(Output):
             elif item == 'samplers':
                 self.samplers = BDSimOutput.Event.Samplers({
                     b.decode('utf-8').rstrip('.'):
-                        BDSimOutput.Event.Sampler(branch=b.decode('utf-8'), tree=self)
-                    for b in self.trees[0].keys() if b.decode('utf-8') not in (
+                        BDSimOutput.Event.Sampler(parent=self, branch_name=b.decode('utf-8'))
+                    for b in self.tree.keys() if b.decode('utf-8') not in (
                         'Summary.',
                         'Primary.',
                         'PrimaryGlobal.',
@@ -699,6 +599,9 @@ class BDSimOutput(Output):
                         'Primary.',
                     )})
                 return self.samplers
+
+            elif item == 'collimators':
+                ...
 
         class ELoss(Output.Branch):
             pass
@@ -837,23 +740,23 @@ class BDSimOutput(Output):
 
         class Sampler(Output.Branch):
             DEFAULT_LEAVES = {
-                'x',
-                'xp',
-                'y',
-                'yp',
-                'z',
-                'zp',
-                'T',
-                'p',
-                'energy',
-                'p',
-                'turnNumber',
-                'parentID',
-                'partID',
-                'trackID',
-                'weight',
-                'n',
-                'S',
+                'x': [True, None],
+                'xp': [True, None],
+                'y': [True, None],
+                'yp': [True, None],
+                'z': [True, None],
+                'zp': [True, None],
+                'T': [True, None],
+                'p': [True, None],
+                'energy': [True, None],
+                'p': [True, None],
+                'turnNumber': [True, None],
+                'parentID': [True, None],
+                'partID': [True, None],
+                'trackID': [True, None],
+                'weight': [True, None],
+                'n': [True, None],
+                'S': [True, None],
             }
 
             def to_np(self,
