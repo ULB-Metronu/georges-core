4c2ab50ea8552fc3d1d09fc7900e3dd32667804c
Cédric Hernalsteens <cedric.hernalsteens@ulb.be>
2020-04-21 17:40:13 +0200

    Minor (matplotlib and fluka)
    Signed-off-by: Cédric Hernalsteens <cedric.hernalsteens@ulb.be>

--- georges_core/vis/matplotlib.py
+++ georges_core/vis/matplotlib.py
@@ -1,7 +1,16 @@
 """
 TODO
 """
+import matplotlib.colors
 from .artist import Artist as _Artist
+
+FLUKA_COLORS = [(1.0, 1.0, 1.0), (0.9, 0.6, 0.9), (1.0, 0.4, 1.0), (0.9, 0.0, 1.0),
+                (0.7, 0.0, 1.0), (0.5, 0.0, 0.8), (0.0, 0.0, 0.8),
+                (0.0, 0.0, 1.0), (0.0, 0.6, 1.0), (0.0, 0.8, 1.0), (0.0, 0.7, 0.5),
+                (0.0, 0.9, 0.2), (0.5, 1.0, 0.0), (0.8, 1.0, 0.0),
+                (1.0, 1.0, 0.0), (1.0, 0.8, 0.0), (1.0, 0.5, 0.0), (1.0, 0.0, 0.0),
+                (0.8, 0.0, 0.0), (0.6, 0.0, 0.0), (0.0, 0.0, 0.0)]
+FlukaColormap = matplotlib.colors.LinearSegmentedColormap.from_list('fluka', FLUKA_COLORS, N=300)
 
 
 class MatplotlibArtist(_Artist):
