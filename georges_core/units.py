fb517e1d75fd66fe14a9be25efc9638b2870d8a6
Robin Tesse <robin.tesse@ulb.be>
2020-06-08 10:47:02 +0200

    Define Gauss as a unit

--- georges_core/units.py
+++ georges_core/units.py
@@ -9,3 +9,4 @@ Q_ = ureg.Quantity
 ureg.define('electronvolt = e * volt = eV')
 ureg.define('electronvolt_per_c = eV / c = eV_c')
 ureg.define('electronvolt_per_c2 = eV / c**2 = eV_c2')
+ureg.define('gauss = 1e-4 * tesla = G')  # see https://github.com/hgrecco/pint/issues/1105
