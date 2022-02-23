207017648504924f2bfa2fa7299b0a98a288882d
Robin Tesse <robin.tesse@ulb.be>
2022-02-23 09:42:56 +0100

    Add FIXME

--- georges_core/sequences/sequence.py
+++ georges_core/sequences/sequence.py
@@ -994,6 +994,8 @@ class BDSIMSequence(Sequence):
         self.set_units(bdsim_model)
 
         # Load the beam properties
+        # FIXME Change the method to access beam base. Do not change in pybdsim
+
         bdsim_beam = bdsim_data.beam.beam_base.pandas(branches=['beamEnergy', 'particle'])
         particle_name = bdsim_beam["particle"].values[0].capitalize()
         particle_energy = bdsim_beam["beamEnergy"].values[0] * _ureg.GeV
