b5e95e45ae1696881bb881a2c6d00ef6fc887139
Robin Tesse <robin.tesse@ulb.be>
2022-03-03 08:54:24 +0100

    Minor changes

--- georges_core/sequences/sequence.py
+++ georges_core/sequences/sequence.py
@@ -846,58 +846,32 @@ class SurveySequence(Sequence):
             metadata: metadata of the sequence
         """
 
-        def compute_at_position():
-            def get_entrance_exit_frame(e):
-                # TODO check if it works in 3D
-                if e['TYPE'] == 'SBEND':
-                    Angle = -_np.pi / 2 - e['ANGLE'] / 2 - e['CUMULATIVE_ANGLE']
-                    x_c = e['X'] + _np.sign(e['ANGLE']) * e['RADIUS'] * _np.cos(Angle)
-                    y_c = e['Y'] + _np.sign(e['ANGLE']) * e['RADIUS'] * _np.sin(Angle)
-                    dx = e['X'] - x_c
-                    dy = e['Y'] - y_c
-                    frame_bend_center = Frame().translate([x_c * _ureg.m, y_c * _ureg.m, 0 * _ureg.m])
-                    f1 = copy.deepcopy(Frame(frame_bend_center).translate([dx * _ureg.m, dy * _ureg.m, 0 * _ureg.m]))
-                    f2 = copy.deepcopy(Frame(frame_bend_center).translate([dx * _ureg.m, dy * _ureg.m, 0 * _ureg.m]))
-                    e['F_ENTRANCE'] = Frame().translate(
-                        [f1.rotate_z(e['ANGLE'] / 2 * _ureg.radians).x, f1.rotate_z(e['ANGLE'] / 2 * _ureg.radians).y,
-                         f1.z])
-                    e['F_EXIT'] = Frame().translate(
-                        [f2.rotate_z(-e['ANGLE'] / 2 * _ureg.radians).x, f2.rotate_z(-e['ANGLE'] / 2 * _ureg.radians).y,
-                         f2.z])
-                else:
-                    frame_ele_center = Frame().translate([e['X'] * _ureg.m, e['Y'] * _ureg.m, e['Z'] * _ureg.m])
-                    e['F_ENTRANCE'] = Frame(frame_ele_center).translate_x([-e['L'] / 2] * _ureg.m).rotate_z(
-                        -e['CUMULATIVE_ANGLE'] * _ureg.radians)
-                    e['F_EXIT'] = Frame(frame_ele_center).translate_x([e['L'] / 2] * _ureg.m).rotate_z(
-                        -e['CUMULATIVE_ANGLE'] * _ureg.radians)
-                return e
+        def get_entrance_exit_frame(e):
+            center_frame = Frame().translate([e['X'] * _ureg.meter, e['Y'] * _ureg.meter, e['Z'] * _ureg.meter])
+            if e['TYPE'] == 'SBEND':
+                # TODO This is not working in 3D and avoid copy.
+                Angle = -_np.pi / 2 - e['ANGLE'] / 2 - e['CUMULATIVE_ANGLE']
+                radius = _np.abs(e['L'] / e['ANGLE'])
+                x_c = e['X'] + _np.sign(e['ANGLE'].m_as('radians')) * radius.m_as('m') * _np.cos(Angle.m_as('radians'))
+                y_c = e['Y'] + _np.sign(e['ANGLE'].m_as('radians')) * radius.m_as('m') * _np.sin(Angle.m_as('radians'))
+                dx = e['X'] - x_c
+                dy = e['Y'] - y_c
+                frame_bend_center = Frame().translate([x_c * _ureg.m, y_c * _ureg.m, 0 * _ureg.m])
+                f1 = copy.deepcopy(Frame(frame_bend_center).translate([dx * _ureg.m, dy * _ureg.m, 0 * _ureg.m]))
+                f2 = copy.deepcopy(Frame(frame_bend_center).translate([dx * _ureg.m, dy * _ureg.m, 0 * _ureg.m]))
 
-            sequence['RADIUS'] = _np.abs(sequence['L'] / sequence['ANGLE'])
-            sequence['CUMULATIVE_ANGLE'] = sequence['ANGLE'].cumsum().shift(1)
-            sequence = sequence.apply(lambda e: get_entrance_exit_frame(e), axis=1)
-
-            # Insert a marker at entrance and exit that have the same frame
-            marker_start = {'TYPE': 'MARKER', 'X': 0, 'Y': 0, 'Z': 0, 'L': 0, 'F_ENTRANCE': Frame(),
-                            'F_EXIT': Frame()}
-            marker_end = {'TYPE': 'MARKER', 'X': sequence.iloc[-1]['X'], 'Y': sequence.iloc[-1]['Y'],
-                          'Z': sequence.iloc[-1]['Z'], 'L': 0, 'F_ENTRANCE': sequence.iloc[-1]['F_EXIT'],
-                          'F_EXIT': sequence.iloc[-1]['F_EXIT']}
-
-            sequence = _pd.concat([_pd.DataFrame(marker_start, index=["$START"]),
-                                   sequence,
-                                   _pd.DataFrame(marker_end, index=["$END"])],
-                                  ignore_index=False)
-
-            sequence['F_EXIT'] = sequence['F_EXIT'].shift(1)
-            sequence.loc["$START", 'F_EXIT'] = Frame()
-            at = 0
-            for i, j in sequence.iterrows():
-                d = _np.linalg.norm(_np.array([(j['F_ENTRANCE'].x - j['F_EXIT'].x).m_as('m'),
-                                               (j['F_ENTRANCE'].y - j['F_EXIT'].y).m_as('m'),
-                                               (j['F_ENTRANCE'].z - j['F_EXIT'].z).m_as('m')]))
-                at += (d + j['L'] / 2)
-                sequence.at[i, 'AT_CENTER'] = at
-                at += j['L'] / 2
+                frame_entrance = Frame().translate(
+                    [f1.rotate_z(e['ANGLE'] / 2 * _ureg.radians).x, f1.rotate_z(e['ANGLE'] / 2 * _ureg.radians).y,
+                     f1.z])
+                frame_exit = Frame().translate(
+                    [f2.rotate_z(-e['ANGLE'] / 2 * _ureg.radians).x, f2.rotate_z(-e['ANGLE'] / 2 * _ureg.radians).y,
+                     f2.z])
+            else:
+                frame_entrance = Frame(center_frame).translate_x(-e['L'] / 2).rotate_z(
+                    -e['CUMULATIVE_ANGLE'])
+                frame_exit = Frame(center_frame).translate_x(e['L'] / 2).rotate_z(
+                    -e['CUMULATIVE_ANGLE'])
+            return frame_entrance, frame_exit
 
         sequence = _pd.read_csv(os.path.join(path, filename), index_col='NAME', sep=',')
         sequence['L'] = sequence['L'].fillna(0).apply(lambda e: e * _ureg.meter)
@@ -905,7 +879,19 @@ class SurveySequence(Sequence):
 
         # check if the survey is (AT_CENTER, L) or (X, Y, Z)
         if sequence.get(['AT_CENTER']) is None and sequence.get(['X', 'Y', 'Z']) is not None:
-            compute_at_position()
+            sequence['CUMULATIVE_ANGLE'] = sequence['ANGLE'].cumsum().shift(1)
+            sequence.loc[sequence.iloc[0].name, 'CUMULATIVE_ANGLE'] = [0.0] * _ureg.radians
+            sequence[['F_ENTRANCE', 'F_EXIT']] = sequence.apply(lambda e: get_entrance_exit_frame(e), axis=1,
+                                                                result_type='expand')
+            at = 0
+            for i, j in sequence.iterrows():
+                d = _np.linalg.norm(_np.array([(j['F_ENTRANCE'].x - j['F_EXIT'].x).m_as('m'),
+                                               (j['F_ENTRANCE'].y - j['F_EXIT'].y).m_as('m'),
+                                               (j['F_ENTRANCE'].z - j['F_EXIT'].z).m_as('m')]))
+                at += (d + j['L'].m_as('m') / 2)
+                sequence.at[i, 'AT_CENTER'] = at
+                at += j['L'].m_as('m') / 2
+
         elif sequence.get(['AT_CENTER']) is not None and sequence.get(['X', 'Y', 'Z']) is None:
             pass
         else:
